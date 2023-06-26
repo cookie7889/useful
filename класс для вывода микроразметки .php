11ace7ac909f125c78671b7b815d8370e5a6a523
cookie7889 <111374909+cookie7889@users.noreply.github.com>
2023-06-26 12:00:21 +0300

    Update класс для вывода микроразметки

--- класс для вывода микроразметки .php
+++ класс для вывода микроразметки .php
@@ -1,37 +1,48 @@
 <? class customSchema
 {
-	private $name;
-	private $description;
-	private $img_src;
-	private $img_width;
-	private $img_height;
+    private $name;
+    private $description;
+    private $img_src;
+    private $img_width;
+    private $img_height;
 
-	public function __construct($name, $description, $img_src, $img_width, $img_height)
-	{
-		$this->name = $name;
-		$this->description = $description;
-		$this->img_src = $img_src;
-		$this->img_width = $img_width;
-		$this->img_height = $img_height;
+    public function __construct($object = [
+        'name', 'description', 'img_src', '$img_width', '$img_heigh'
+    ])
+    {
+        $this->name = $object['name'];
+        $this->description = $object['description'];
+        $this->img_src = $object['img_src'];
+        $this->img_width = $object['img_width'];
+        $this->img_height = $object['img_height'];
+    }
 
-	}
+    public function ImageObject()
+    {
+        echo '<div style="display: none;" itemscope itemtype="http://schema.org/ImageObject">';
+        echo '<meta itemprop="name" content="' . $this->name . '">';
+        echo '<link src="' . $this->img_src . '" itemprop="contentUrl" />';
 
-	public function ImageObject()
-	{
-		echo '
-			<div style="display: none;" itemscope itemtype="http://schema.org/ImageObject">
-				<meta itemprop="name" content="' . $this->name . '">
-				<link src="' . $this->img_src . '" itemprop="contentUrl" />
-				<meta itemprop="description" content="' . $this->description . '">
-				<meta itemprop="width" content="' . $this->img_width . '">
-				<meta itemprop="height" content="' . $this->img_height . '">
-			</div>
-		';
-	}
+        if (!empty($this->description))
+            echo '<meta itemprop="description" content="' . $this->description . '">';
+        if (!empty($this->img_width))
+            echo '<meta itemprop="width" content="' . $this->img_width . '">';
+        if (!empty($this->img_height))
+            echo '<meta itemprop="height" content="' . $this->img_height . '">';
+
+        echo '</div>';
+    }
 }
 
 
 //пример вывода 
-$customSchema = new customSchema($arResult['NAME'], $arResult['IPROPERTY_VALUES']['ELEMENT_META_TITLE'], $arResult['DETAIL_PICTURE']['SRC'], $arResult['DETAIL_PICTURE']['WIDTH'], $arResult['DETAIL_PICTURE']['HEIGHT'], );
-
+$customSchema = new customSchema(
+    array(
+        'name' => $arResult['NAME'],
+        'description' => $arResult['PREVIEW_TEXT'],
+        'img_src' => $img['SRC'],
+        'img_width' => $img['WIDTH'],
+        'img_heigh' => $img['HEIGHT'],
+    )
+);
 $customSchema->ImageObject();
