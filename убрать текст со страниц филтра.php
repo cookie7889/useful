19bb008cf4d11bfde208551bfdf1c09dae1b3b43
cookie7889 <111374909+cookie7889@users.noreply.github.com>
2023-07-21 16:38:17 +0300

    Update убрать текст со страниц филтра.php

--- убрать текст со страниц филтра.php
+++ убрать текст со страниц филтра.php
@@ -1,27 +1,27 @@
 <?# /bitrix/templates/aspro_max/components/bitrix/catalog/main/page_blocks  745 ?>
 <? ob_start(); ?>
 <? if ($arResult['FOLDER'] . $arResult['VARIABLES']['SECTION_CODE_PATH'] . '/' == $APPLICATION->GetCurPage()) : ?>
-	<? if ($posSectionDescr == "BOTH") : ?>
-		<? if ($arSection[$section_pos_bottom]) : ?>
-			<div class="group_description_block bottom muted777">
-				<div><?= $arSection[$section_pos_bottom] ?></div>
-			</div>
-		<? endif; ?>
-	<? elseif ($posSectionDescr == "BOTTOM") : ?>
-		<? if ($arSection[$arParams["SECTION_PREVIEW_PROPERTY"]]) : ?>
-			<div class="group_description_block bottom muted777">
-				<div><?= $arSection[$arParams["SECTION_PREVIEW_PROPERTY"]] ?></div>
-			</div>
-		<? elseif ($arSection["DESCRIPTION"]) : ?>
-			<div class="group_description_block bottom muted777">
-				<div><?= $arSection["DESCRIPTION"] ?></div>
-			</div>
-		<? elseif ($arSection["UF_SECTION_DESCR"]) : ?>
-			<div class="group_description_block bottom muted777">
-				<div><?= $arSection["UF_SECTION_DESCR"] ?></div>
-			</div>
-		<? endif; ?>
-	<? endif; ?>
+	#<? if ($posSectionDescr == "BOTH") : ?>
+	#	<? if ($arSection[$section_pos_bottom]) : ?>
+	#		<div class="group_description_block bottom muted777">
+	#			<div><?= $arSection[$section_pos_bottom] ?></div>
+	#		</div>
+	#	<? endif; ?>
+	#<? elseif ($posSectionDescr == "BOTTOM") : ?>
+	#	<? if ($arSection[$arParams["SECTION_PREVIEW_PROPERTY"]]) : ?>
+	#		<div class="group_description_block bottom muted777">
+	#			<div><?= $arSection[$arParams["SECTION_PREVIEW_PROPERTY"]] ?></div>
+	#		</div>
+	#	<? elseif ($arSection["DESCRIPTION"]) : ?>
+	#		<div class="group_description_block bottom muted777">
+	#			<div><?= $arSection["DESCRIPTION"] ?></div>
+	#		</div>
+	#	<? elseif ($arSection["UF_SECTION_DESCR"]) : ?>
+	#		<div class="group_description_block bottom muted777">
+	#			<div><?= $arSection["UF_SECTION_DESCR"] ?></div>
+	#		</div>
+	#	<? endif; ?>
+	#<? endif; ?>
 <? endif; ?>
 
 <?
